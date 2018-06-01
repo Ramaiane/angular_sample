3d1505e0a15aecfd4052e7e89d40d5fbb219d9c2
ramaiane_ms <ramaiane.ms@gmail.com>
2018-06-01 17:57:31 -0300

    consumindo uma api e melhorando aparencia

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,9 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent} from './app.component';
 import {FotoModule} from './foto/foto.module';
 import {HttpModule, Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {PainelModule} from './painel/painel.module';
+import {} from './cadastro/cadastro.component';
 
 @NgModule({
-    imports: [ BrowserModule, FotoModule, HttpModule],
+    imports: [ BrowserModule, FotoModule, HttpModule, PainelModule],
     declarations: [ AppComponent],
     bootstrap: [ AppComponent]
 })
